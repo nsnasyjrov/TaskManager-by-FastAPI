64fdb3a71fb4c9f3d9e7ef5004e83d316e7227c1
Nail <nsnasyjrov@gmail.com>
2025-11-03 23:49:56 +0300

    feat(api/routes/user_routes): fixed validation Json and added Pydantic validation

--- app/api/user_routes/user_auth.py
+++ app/api/user_routes/user_auth.py
@@ -1,7 +1,8 @@
-import json
 from fastapi import APIRouter, Request
-from starlette.responses import JSONResponse
+from pydantic_core import ValidationError
 
+from app.schemas.user.user_schema import UserCreateValidateSchema
+from app.utils.common_methods import pydantic_errors
 from app.utils.response_classes import ResultFromHandler
 
 user_auth_router = APIRouter(prefix="/auth", tags=["users"])
@@ -23,11 +24,16 @@ async def authorization():
 @user_auth_router.post(("/register"))
 async def register(request: Request):
     try:
-        user_schema = await request.json()
-    except json.JSONDecodeError as e:
-        return ResultFromHandler.error(None, "Error validate data from client", 400)
+        json_data = await request.json()
+        validate_schema = UserCreateValidateSchema(**json_data)
+    except ValidationError as e:
+        print("Пайдентик нашел ошибку")
+        return ResultFromHandler.error(pydantic_errors(e), "Error when validate from client", 400)
+    except Exception as e:
+        return ResultFromHandler.error(e.__dict__, "Error when validate from client", 400)
+
 
     user_service = request.state.container.get_layer(exp_entity)
-    user = await user_service.create_user(user_schema)
+    #user = await user_service.create_user(user_schema)
 
-    return {"message": "User created", "user_id": user.id}
+    #return {"message": "User created", "user_id": user.id}
