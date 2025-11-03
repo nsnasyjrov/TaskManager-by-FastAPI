0c9d759df33745da61c809a369f106a73cd71320
Nail <nsnasyjrov@gmail.com>
2025-11-03 22:49:56 +0300

    feat(utils): created founder for files & get_layer for container in database.py

--- app/core/database.py
+++ app/core/database.py
@@ -1,9 +1,11 @@
+from typing import Dict, Any, Type
+
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
+from app.core.logger import logger
 from app.core.config import settings
-from app.dal.user_dal import UserDAL
-from app.services.user_service import UserService
+from app.utils.common_methods import get_layers_from_utils
 
 engine = create_async_engine(settings.database_url, echo=settings.debug)
 
@@ -21,5 +23,18 @@ async def get_db() -> AsyncSession:
 class Container:
     def __init__(self, db: AsyncSession):
         self.db = db
-        self.user_dal = UserDAL(db)
-        self.user_service = UserService(self.user_dal)
+        self._cache = {}
+
+    @property
+    def get_layer(self, exp_entity: str):
+
+        service_class, dal_class = get_layers_from_utils(exp_entity)
+
+        if service_class == -1 or dal_class == -1:
+            logger.error("[app/core/database.py] Container: error when getting layer")
+            return -1
+
+        dal_instance = dal_class(self.db)
+        self._cache[exp_entity] = service_class(dal_instance)
+
+        return self._cache[exp_entity]
---
+++ app/utils/common_methods.py
@@ -0,0 +1,69 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CURRENT_DIR = os.path.dirname(__file__)
+APP_DIR = os.path.dirname(CURRENT_DIR)
+
+DAL_PATH = os.path.join(APP_DIR, os.getenv('DAL_PATH', 'app/dal'))
+SERVICES_PATH = os.path.join(APP_DIR, os.getenv('SERVICES_PATH', 'app/services'))
+
+def find_file(name, path):
+    found_file = None
+    extension = ".py" # Пускай остается,
+                      # на случай если потребуется переиспользовать
+                      # поиск файлов, можно будет создать ENUM и искать файл
+
+    if path == DAL_PATH:
+        filename = f"{name}_dal"
+    elif path == SERVICES_PATH:
+        filename = f"{name}_service"
+    else:
+        return -1
+
+    if filename is not None:
+        filename = filename + extension
+
+        for root, dirs, files in os.walk(path):
+            if filename in files:
+                found_file = os.path.join(root, filename)
+                break
+
+    return found_file
+
+def select_dal(name: str):
+    """Accepts the name of an entity or interaction object from DAL;
+    var name must match: <name>_dal
+    :return: Type"""
+
+    dal_class  =  find_file(name, DAL_PATH)
+
+    return dal_class
+
+def select_service(name: str):
+    """Accepts the name of an entity or interaction object from Services BLL;
+    var name must match: <name>_service
+    :return: Type"""
+
+    service_class = find_file(name, SERVICES_PATH)
+
+    return service_class
+
+def get_layers_from_utils(name: str):
+    service_class = select_service(name)
+    dal_class = select_dal(name)
+
+    if service_class is None or dal_class is None:
+        # if anyone entity is not founded return -1 code
+        # because is error
+        return -1, -1
+
+    return service_class, dal_class
+
+
+
+
+
+
